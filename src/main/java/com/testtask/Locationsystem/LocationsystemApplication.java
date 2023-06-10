40b8d529c05a59e99e529d953fb99c03e2221e64
Yerasyl-zh <rigbyzh@gmail.com>
2023-06-10 14:57:46 +0600

    Initial commit

---
+++ src/main/java/com/testtask/Locationsystem/LocationsystemApplication.java
@@ -0,0 +1,13 @@
+package com.testtask.Locationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testtask/Locationsystem/LocationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testtask.Locationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
